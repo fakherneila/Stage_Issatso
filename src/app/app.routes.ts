ef235996f43a5e6aa29bc8e472c9183c4a90ce25
Fakher Neila <neilafakher8@gmail.com>
2025-08-20 23:10:16 +0100

    add paths , and add "deposer rapport " , "voir soutounance" et "historique

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { FormContainerComponent } from './form-container/form-container';
+import { DeposerRapportComponent } from './deposer-rapport/deposer-rapport';
+import { VoirSoutenanceComponent } from './voir-soutenance/voir-soutenance';
+import { HistoriqueComponent } from './historique/historique';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'remplir-formulaire',
+    component: FormContainerComponent
+  },
+  {
+    path: 'deposer-rapport',
+    component: DeposerRapportComponent
+  },
+  {
+    path: 'voir-soutenance',
+    component: VoirSoutenanceComponent
+  },
+  {
+    path: 'historique',
+    component: HistoriqueComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'remplir-formulaire'
+  }
+];
